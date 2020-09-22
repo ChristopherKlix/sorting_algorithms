5b574702aedd26460af8615a1adf542d4fbb922d
ChristopherKlix <70777673+ChristopherKlix@users.noreply.github.com>
2020-09-22 16:53:40 +0200

    improved merge sort

--- merge_sort/merge_sort.py
+++ merge_sort/merge_sort.py
@@ -1,17 +1,13 @@
 from util import generate_list, print_line, get_timestamp
 
-show_debug = False
-
 
 def sort(unsorted_list):
 
     # if list is large than 1
     if len(unsorted_list) > 1:
 
-        # split list in half
-        half = len(unsorted_list)//2
-        left_half = unsorted_list[:half]
-        right_half = unsorted_list[half:]
+        # split list in halves
+        left_half, right_half = split(unsorted_list)
 
         # sort left half
         sorted_left_half = sort(left_half)
@@ -20,102 +16,74 @@ def sort(unsorted_list):
         sorted_right_half = sort(right_half)
     
         # merge halves
-        sorted_list = []
-
-        for _ in range(len(unsorted_list)):
-
-            # if both lists have elements left
-            if len(sorted_left_half) > 0 and len(sorted_right_half) > 0:
-
-                # if right list has smallest number
-                if sorted_left_half[0] > sorted_right_half[0]:
-                    sorted_list.append(sorted_right_half[0])
-                    sorted_right_half.pop(0)
-
-                # if left list has smallest number
-                elif sorted_left_half[0] < sorted_right_half[0]:
-                    sorted_list.append(sorted_left_half[0])
-                    sorted_left_half.pop(0)
-
-                # if both lists have the same smallest number
-                # use left list
-                else:
-                    sorted_list.append(sorted_left_half[0])
-                    sorted_left_half.pop(0)
-
-            # else if only one list has element left
-            else:
-                if len(sorted_left_half) > 0:
-                    sorted_list.append(sorted_left_half[0])
-                    sorted_left_half.pop(0)
-                else:
-                    sorted_list.append(sorted_right_half[0])
-                    sorted_right_half.pop(0)
+        sorted_list = merge(sorted_left_half, sorted_right_half)
         
         # return sorted list
         return sorted_list
     
     # if list is only one number
     else:
-        return unsorted_list
-
-
-def debug_sort(unsorted_list):
-    print(f'---------------- sort {unsorted_list} ----------------')
-    if len(unsorted_list) > 1:
-        # split list in half
-        if show_debug:
-            print('split')
-        half = len(unsorted_list)//2
-        left_half = unsorted_list[:half]
-        right_half = unsorted_list[half:]
-
-        # sort left half
-        sorted_left_half = sort(left_half)
-        if show_debug:
-            print(f'sorted left half: {sorted_left_half}')
-
-        # sort right half
-        sorted_right_half = sort(right_half)
-        if show_debug:
-            print(f'sorted right half: {sorted_right_half}')
-    
-        # merge halves
-        if show_debug:
-            print(f'--- merge {sorted_left_half} and {sorted_right_half} ---')
-        sorted_list = []
-
-        for i in range(len(unsorted_list)):
-            if len(sorted_left_half) > 0 and len(sorted_right_half) > 0:
-                if sorted_left_half[0] > sorted_right_half[0]:
-                    sorted_list.append(sorted_right_half[0])
-                    sorted_right_half.pop(0)
-
-                elif sorted_left_half[0] < sorted_right_half[0]:
-                    sorted_list.append(sorted_left_half[0])
-                    sorted_left_half.pop(0)
-
-                else:
-                    sorted_list.append(sorted_left_half[0])
-                    sorted_left_half.pop(0)
-
-            elif len(sorted_left_half) > 0:
-                sorted_list.append(sorted_left_half[0])
-                sorted_left_half.pop(0)
-
-            else:
-                sorted_list.append(sorted_right_half[0])
-                sorted_right_half.pop(0)
-            if show_debug:
-                print(sorted_list)
-        
-        # return sorted list
-        if show_debug:
-            print(f'merged: {sorted_list}')
+        # this is just for clarification
+        sorted_list = unsorted_list
         return sorted_list
-    
-    else:
-        return unsorted_list
+
+
+def split(full_list):
+    '''
+    get length of list
+    initialize both halves
+    '''
+    list_len = len(full_list)
+    left_half, right_half = list(), list()
+
+    '''
+    iterate over each item in full_list
+    and append to left half until i is greater than length / 2
+    '''
+    for i in range(list_len):
+        if i < list_len / 2:
+            left_half.append(full_list[i])
+        else:
+            right_half.append(full_list[i])
+
+    return left_half, right_half
+
+
+def merge(left_half, right_half):
+    merged_list = list()
+    both_halves_len = len(left_half) + len(right_half)
+
+    for _ in range(both_halves_len):
+
+            # if both lists have elements left
+            if len(left_half) > 0 and len(right_half) > 0:
+
+                # if right list has smallest number
+                if left_half[0] > right_half[0]:
+                    merged_list.append(right_half[0])
+                    right_half.pop(0)
+
+                # if left list has smallest number
+                elif left_half[0] < right_half[0]:
+                    merged_list.append(left_half[0])
+                    left_half.pop(0)
+
+                # if both lists have the same smallest number
+                # use left list
+                else:
+                    merged_list.append(left_half[0])
+                    left_half.pop(0)
+
+            # else if only one list has element left
+            else:
+                if len(left_half) > 0:
+                    merged_list.append(left_half[0])
+                    left_half.pop(0)
+                else:
+                    merged_list.append(right_half[0])
+                    right_half.pop(0)
+
+    return merged_list
 
 
 def main():
@@ -142,10 +110,7 @@ def main():
     # sort list
     timestamp_start = get_timestamp()
 
-    if show_debug:
-        sorted_list = debug_sort(unsorted_list)
-    else:
-        sorted_list = sort(unsorted_list)
+    sorted_list = sort(unsorted_list)
         
     timestamp_finish = get_timestamp()
 
